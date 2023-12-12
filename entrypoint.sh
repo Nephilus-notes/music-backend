fe523a00c5bafd26284f799fced07e8ac476082f
Nephilus-notes <114515979+Nephilus-notes@users.noreply.github.com>
2023-12-12 12:24:27 -0600

    added dockerfile, docker compose, entrypoint.sh, and adjusted env settings

---
+++ entrypoint.sh
@@ -0,0 +1,8 @@
+
+
+python manage.py migrate -- no-input
+python manage.py collectstatic --no-input 
+
+gunicorn musicbackend.wsgi:application --bind 0.0.0.0:8000
+
+
--- musicbackend/musicbackend/settings.py
+++ musicbackend/musicbackend/settings.py
@@ -25,12 +25,12 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-sn9$0=#7c9%kbt92pp_m)54bwl8r1th1vbpe#8y9m&15=-=3j%'
+SECRET_KEY = env.str("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = env('DEBUG')
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
